a8039d896acf798c9d9c19d46bc4778f3125dbe9
Liam Fruzyna <liamfruzyna@gmail.com>
2021-02-07 12:38:08 -0600

    Add all available values to workbook

--- podcast.py
+++ podcast.py
@@ -14,17 +14,25 @@ root = ET.fromstring(rss)
 items = root.findall('.//item')
 items.reverse()
 
+# get unique tags
+tags = [e.tag for e in items[0].iter() if e.tag != 'item']
+names = [t if '}' not in t else t[t.rindex('}')+1:] for t in tags]
+
 # create workbook
 wb = Workbook()
 sheet = wb.active
-sheet.append(['Title', 'Date', 'Length (mins)'])
+sheet.append(names)
 
 # move data from XML to workbook
 for idx, item in enumerate(items):
-	title = item.find('title').text.split(' - ')[0]
-	date = item.find('pubDate').text
-	length = float(item.find('{http://www.itunes.com/dtds/podcast-1.0.dtd}duration').text)/60
-	sheet.append([title, date, length])
+	row = []
+	for tag in tags:
+		if item.find(tag) != None:
+			value = item.find(tag).text
+			if tag.endswith('duration'):
+				value = float(value) / 60
+			row.append(value)
+	sheet.append(row)
 
 # save workbook
 title = root.find('channel').find('title').text
