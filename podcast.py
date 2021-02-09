98cdf1d5c40e6f22237f098f0c3d0a5e968a1280
Liam Fruzyna <liamfruzyna@gmail.com>
2021-02-08 19:06:07 -0600

    Draw a plot of duration and variables to control output

--- podcast.py
+++ podcast.py
@@ -1,7 +1,14 @@
 import xml.etree.ElementTree as ET
 from openpyxl import Workbook
 from urllib.request import urlopen
+from matplotlib import pyplot as plt
+import numpy as np
 import sys
+
+# placeholder variables
+export = True
+plot = True
+bounds = False
 
 # download podcast from RSS URL
 if len(sys.argv) < 2:
@@ -23,6 +30,9 @@ wb = Workbook()
 sheet = wb.active
 sheet.append(names)
 
+dates = []
+times = []
+
 # move data from XML to workbook
 for idx, item in enumerate(items):
 	row = []
@@ -31,11 +41,55 @@ for idx, item in enumerate(items):
 			value = item.find(tag).text
 			if tag.endswith('duration'):
 				value = float(value) / 60
+				times.append(value)
+			elif tag.endswith('pubDate'):
+				dates.append(value)
 			row.append(value)
 	sheet.append(row)
 
 # save workbook
-title = root.find('channel').find('title').text
-name = title + '.xlsx'
-wb.save(filename=name)
-print('Wrote to', name)
+if export:
+	title = root.find('channel').find('title').text
+	name = title + '.xlsx'
+	wb.save(filename=name)
+	print('Wrote to', name)
+
+if plot:
+	# plot by episode number
+	x = range(1, len(times)+1)
+	plt.plot(x, times)
+
+	# add trendlines
+	z = np.polyfit(x, times, 1)
+	p = np.poly1d(z)
+	plt.plot(x, p(x))
+
+	# add labels
+	plt.title('"{}" Episode Duration vs Time'.format(title))
+	plt.xlabel('Episode Number')
+	plt.ylabel('Length (mins)')
+
+	# draw some unnecessary lines for min and max over time
+	if bounds:
+		maxes = []
+		mins = []
+		maxXs = []
+		minXs = []
+		pos = p(1) > p(0)
+		for i in range(len(times)):
+			val = max(times[0:i+1]) if pos else max(times[i:])
+			if times[i] == val:
+				maxes.append(val)
+				maxXs.append(i+1)
+		for i in reversed(range(len(times))):
+			val = min(times[i:]) if pos else min(times[0:i+1])
+			if times[i] == val:
+				mins.append(val)
+				minXs.append(i+1)
+
+		# plot min/max lines
+		plt.plot(maxXs, maxes)
+		plt.plot(minXs, mins)
+
+	# show plot
+	plt.show()
